a4f9f12a98f65fe09d2cbd0bb5253b3e3b5ebfdc
wangzhengyi <wangzhengyi@meican.com>
2024-06-26 06:34:47 +0800

    task: create OkHttpClient and Retrofit object in NetworkModule

---
+++ app/src/main/java/nz/co/test/transactions/common/Constants.kt
@@ -0,0 +1,5 @@
+package nz.co.test.transactions.common
+
+object Constants {
+    const val BASE_URL = " https://gist.githubusercontent.com/"
+}
--- app/src/main/java/nz/co/test/transactions/di/network/NetworkModule.kt
+++ app/src/main/java/nz/co/test/transactions/di/network/NetworkModule.kt
@@ -1,16 +1,42 @@
 package nz.co.test.transactions.di.network
 
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import nz.co.test.transactions.common.Constants.BASE_URL
 import nz.co.test.transactions.services.TransactionsService
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val moshi = Moshi.Builder()
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
 
     @Singleton
     @Provides
