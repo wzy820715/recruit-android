88b3709f017e9723767da8c0ad032ac00a816537
wangzhengyi <wangzhengyi@meican.com>
2024-06-26 08:09:45 +0800

    task: add unit test

--- app/src/androidTest/java/nz/co/test/transactions/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/nz/co/test/transactions/ExampleInstrumentedTest.kt
@@ -7,6 +7,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +24,20 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("nz.co.test.transactions", appContext.packageName)
+    }
+
+    @Test
+    fun offsetDateTimeConvert(){
+        val string = "2021-01-01T15:38:46"
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+        val localDateTime = LocalDateTime.parse(string, formatter)
+        val offsetDateTime = OffsetDateTime.of(localDateTime, OffsetDateTime.now().offset)
+        assertEquals(true, offsetDateTime.toString().contains(string))
+    }
+
+    @Test
+    fun bigDecimalConvert(){
+        val string = "9379.55"
+        assertEquals("9379.55", BigDecimal(string).toString())
     }
 }
